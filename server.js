3df484e36f98b140dc3200439368cac06775e9be
Lian <lian@Lians-MacBook-Pro-2.local>
2025-03-14 09:40:51 -0500

    Cambios de rutas en server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const session = require('cookie-session');
+const path = require('path');
 const { PORT, SERVER_SESSION_SECRET } = require('./config.js');
 
 let app = express();
@@ -7,5 +8,12 @@ app.use(express.static('wwwroot'));
 app.use(session({ secret: SERVER_SESSION_SECRET, maxAge: 24 * 60 * 60 * 1000 }));
 app.use(require('./routes/auth.js'));
 app.use(require('./routes/hubs.js'));
+// Servir archivos estáticos desde wwwroot
+app.use(express.static(path.join(__dirname, 'wwwroot')));
+
+// Manejo de rutas desconocidas
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'wwwroot', 'index.html'));
+});
 app.use('/pdfs', express.static('public/pdfs'));
 app.listen(PORT, () => console.log(`Server listening on port ${PORT}...`));
